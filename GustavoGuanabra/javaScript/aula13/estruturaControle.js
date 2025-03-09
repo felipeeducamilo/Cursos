edb3e3e6cbbf2e66775911b84b18bbb7659952ee
Felipe Eduardo Camilo Ferreira <157247102+FelipeEduCamilo@users.noreply.github.com>
2025-03-09 19:40:02 -0300

    es

---
+++ GustavoGuanabra/javaScript/aula13/estruturaControle.js
@@ -0,0 +1,14 @@
+// let contador = 1;
+// while (contador < 11) {
+//   console.log(`Esse é passo ${contador}`);
+//   contador++;
+// }
+
+// do {
+//   console.log(`Esse é o passo ${contador}`);
+//   contador++;
+// } while (contador < 11);
+
+for (cont = 1; cont <= 10; cont++) {
+  console.log(`Contando ${cont}`);
+}
