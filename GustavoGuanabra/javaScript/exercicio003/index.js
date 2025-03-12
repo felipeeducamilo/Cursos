c3b0f4ee9ba0f464a6cd2783a5e74df4322eff72
Felipe Eduardo Camilo Ferreira <157247102+FelipeEduCamilo@users.noreply.github.com>
2025-03-12 20:18:45 -0300

    ex

--- GustavoGuanabra/javaScript/exercicio003/index.js
+++ GustavoGuanabra/javaScript/exercicio003/index.js
@@ -2,15 +2,34 @@ function calcular() {
   let inicio = document.getElementById("txtinicio").value;
   let fim = document.getElementById("txtfim").value;
   let intervalo = document.getElementById("txtintervalo").value;
+  let resultado = document.getElementById("resultado");
+  resultado.innerHTML = "";
   if (inicio == "" || fim == "" || intervalo == "") {
     alert("[ERRO] Digite todos os campos para ser calculado");
   } else {
     inicio = Number(inicio);
     fim = Number(fim);
     intervalo = Number(intervalo);
-    for (cont = inicio; cont <= fim; cont += intervalo) {
-      let resultado = document.getElementById("resultado");
-      resultado.innerHTML += `  ${cont} \u{1F600}`;
+    if (intervalo < 1) {
+      intervalo = 1 ;
+      alert("O intervalo mínimo será 1");
     }
+    if (inicio < fim) {
+      for (cont = inicio; cont <= fim; cont += intervalo) {
+        resultado.innerHTML += `  ${cont} \u{1f449}`;
+      }
+    } else {
+      for (cont = inicio; cont >= fim; cont -= intervalo) {
+        resultado.innerHTML += ` ${cont} \u{1f449}`;
+      }
+    }
+    resultado.innerHTML += ` \u{1f3c1}`;
   }
+}
+
+function limpar() {
+  document.getElementById("txtinicio").value = "";
+  document.getElementById("txtfim").value = "";
+  document.getElementById("txtintervalo").value = "";
+  document.getElementById("resultado").innerHTML = "";
 }
