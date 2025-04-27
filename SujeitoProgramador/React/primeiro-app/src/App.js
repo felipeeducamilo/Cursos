0e9115a28da98e61f55ff500d5e2dd6cea8b4f05
Felipe Eduardo Camilo Ferreira <157247102+FelipeEduCamilo@users.noreply.github.com>
2025-04-26 22:49:54 -0300

    react

--- SujeitoProgramador/React/primeiro-app/src/App.js
+++ SujeitoProgramador/React/primeiro-app/src/App.js
@@ -15,61 +15,17 @@
 // import { useState } from "react";
 
 // function App() {
-//   const [aluno, setAluno] = useState("Eduardo");
+//   const [aluno, setAluno] = useState("Felipe Eduardo");
 
 //   function handleChangeName(nome) {
-//     alert("Clicou");
 //     setAluno(nome);
 //   }
+
 //   return (
 //     <div>
-//       <h1> Componente App </h1>
+//       <h1>Componente App</h1>
 //       <br />
-//       <h2> Olá: {aluno} </h2>
-//       <button onClick={() => handleChangeName("Dumont")}>Mudar Nome</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import { useState } from "react";
-
-// function App() {
-//   const [aluno, setAluno] = useState("Felipe Eduardo");
-
-//   const handleChangeName = () => {
-//     alert("Clicou!");
-//     setAluno("Santos Dumont");
-//   };
-
-//   return (
-//     <div>
-//       <h1>Componente App</h1>
 //       <h2>Olá: {aluno}</h2>
-//       <button onClick={handleChangeName}>Mudar Nome</button>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-// import { useState } from "react";
-
-// function App() {
-//   const [aluno, setAluno] = useState("Felipe Eduardo");
-
-//   function handleChangeName(novoNome) {
-//     alert("Clicou");
-//     console.log("Aluno atual:", aluno);
-//     setAluno(novoNome);
-//   }
-
-//   return (
-//     <div>
-//       <h1>Componente App</h1>
-//       <h2>Olá: {aluno}</h2>
-
 //       <button onClick={() => handleChangeName("Santos Dumont")}>
 //         Mudar Nome
 //       </button>
@@ -79,26 +35,104 @@
 
 // export default App;
 
+// import { useState } from "react";
+
+// function App() {
+//   const [nome, setNome] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [idade, setIdade] = useState("");
+
+//   const [user, setUser] = useState({});
+
+//   function handleRegister(e) {
+//     e.preventDefault();
+
+//     setUser({
+//       nome: nome,
+//       email: email,
+//       idade: idade,
+//     });
+//     alert("Usuário cadastrado com sucesso");
+//   }
+//   return (
+//     <div>
+//       <h1> Cadastro de usuário</h1>
+
+//       <form onSubmit={handleRegister}>
+//         <label>Nome: </label> <br />
+//         <input
+//           placeholder="Digite seu nome"
+//           value={nome}
+//           onChange={(e) => setNome(e.target.value)}
+//         ></input>
+//         <br /> <br />
+//         <label>E-mail: </label> <br />
+//         <input
+//           placeholder="Digite seu e-mail"
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//         ></input>
+//         <br /> <br />
+//         <label>Idade: </label> <br />
+//         <input
+//           placeholder="Digite sua idade"
+//           value={idade}
+//           onChange={(e) => setIdade(e.target.value)}
+//         ></input>
+//         <br /> <br />
+//         <button type="submit">Registrar</button> <br />
+//       </form>
+
+//       <br />
+
+//       <div>
+//         <span> Nome: {user.nome}</span> <br />
+//         <span> Idade: {user.idade}</span> <br />
+//         <span> E-mail: {user.email} </span> <br />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 import { useState } from "react";
 
 function App() {
-  const [aluno, setAluno] = useState("Felipe Eduardo");
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState([
+    "comprar coca-cola",
+    "estudar react Js",
+  ]);
 
-  function handleChangeName(nome) {
-    console.log("Nome antes de mudar: ", aluno); // Verifique o nome no console
-    setAluno(nome);
-    console.log("Nome depois de mudar: ", nome); // Verifique se o nome foi realmente alterado
+  function handleRegister(e) {
+    e.preventDefault();
+
+    setTarefas([...tarefas, input]);
+    setInput("");
   }
-
   return (
     <div>
-      <h1>Componente App</h1>
+      <h1> Cadastro de usuário</h1>
+
+      <form onSubmit={handleRegister}>
+        <label>Nome: </label> <br />
+        <input
+          placeholder="Digite uma tarefa"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        ></input>
+        <br /> <br />
+        <button type="submit">Registrar</button> <br />
+      </form>
+
       <br />
-      <h2>Olá: {aluno}</h2>
-      <button onClick={() => handleChangeName("Santos Dumont")}>
-        Mudar Nome
-      </button>
+
+      <ul>
+        {tarefas.map((tarefa) => (
+          <li key={tarefa}> {tarefa}</li>
+        ))}
+      </ul>
     </div>
   );
 }
